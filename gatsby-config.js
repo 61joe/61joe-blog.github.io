a0c6820fa1f881b14e897f05285fe724f63bce3a
61joe <705074065@qq.com>
2022-03-25 17:44:04 +0800

    测试远程地址

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-// haha
+// https://github.com/61joe/61joe-blog.github.io.git
 module.exports = {
   /* Your site config here */
   plugins: [],
